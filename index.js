ae8fa030cee0111de6dbc4eca1b18291ab6ce85a
Gitithub321 <52983322+Gitithub321@users.noreply.github.com>
2023-06-12 21:53:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/openai", require("./routes/openaiRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
